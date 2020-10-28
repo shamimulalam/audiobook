740d59ef00313cbc93fa4ee4b2ffe166d5283e7e
Shamimul Alam <bd.shawon1991@gmail.com>
2020-10-28 23:46:51 +0600

    File added

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import pyttsx3
 import PyPDF2
-book = open('oop.pdf', 'rb')
+book = open('ebook.pdf', 'rb')
 pdfReader = PyPDF2.PdfFileReader(book)
 pages = pdfReader.numPages
 
 speaker = pyttsx3.init()
-for num in range(7, pages):
+speaker.say('Reading starting')
+for num in range(20, pages):
     page = pdfReader.getPage(num)
     text = page.extractText()
     speaker.say(text)
     speaker.runAndWait()
+    speaker.say('Page end. Starting new page')
